239c1f4b08f6efb1560435abed8f165867c7cfd4
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-28 19:50:36 -0600

    completed 'my_each_with_index' and 'my_select' methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -2,6 +2,24 @@ require 'pry-byebug'
 
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |element|
+      yield(element, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    result = []
+    #binding.pry
+    my_each do |element|
+      if yield(element)
+        result.push(element)
+      end
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
